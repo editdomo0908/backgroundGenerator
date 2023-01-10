cd8f6731b238be0ce030e919ffeae71b8f6f1c22
editdomo0908 <42967377+editdomo0908@users.noreply.github.com>
2023-01-10 13:36:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 var button = document.querySelector("button");
+var switchButton= document.getElementById("switch");
 var hexValues = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']; 
 
 
@@ -15,6 +17,17 @@ window.addEventListener('load', function () {
 
 
 
+function switchColors() {
+	var a = color1.value;
+	var b = color2.value;
+	color1.value = b;
+	color2.value = a;
+
+	setGradient();
+}
+
+
+
 function setGradient(){
 	body.style.background = "linear-gradient(to right," 
  	+color1.value +", " 
@@ -22,29 +35,6 @@ function setGradient(){
 
      css.textContent = body.style.background + ",";
 }
-
-
-function changeHex () {
-	var hex = "#";
-	for (var i=0; i<6; i++){
-    
-    var hexValuesItem = Math.floor(Math.random()* hexValues.length);
-	}
-	
-	hex += hexValues[hexValuesItem];
-
-	color1.value = hex;
-	color2.value = hex;
-
-}
-
-
- color1.addEventListener("input", setGradient);
-
- color2.addEventListener("input", setGradient);
-
-
-
 
 function changeHex () {
 	var hex1 = "#";
@@ -67,20 +57,15 @@ for (var i=0; i<6; i++){
 
     color2.value=hex2
   
-
-
  
 	body.style.background = "linear-gradient(to right," 
  	+color1.value+", " 
  	+ color2.value + ")" 
 
-
 }
 
-
-
-
 button.addEventListener('click', changeHex);
-
-
+switchButton.addEventListener('click', switchColors)
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
 
